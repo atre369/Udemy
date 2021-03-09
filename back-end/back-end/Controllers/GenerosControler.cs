961911cff5535eacc6d2afb60141780c7c0b7359
Atreyu <moralesatreyu@hotmail.com>
2021-03-08 21:54:57 -0600

    add transient error

---
+++ back-end/back-end/Controllers/GenerosControler.cs
@@ -0,0 +1,18 @@
+﻿using back_end.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Controllers
+{
+    public class GenerosControler
+    {
+        private readonly IRepositorio repositorio;
+        public GenerosControler(IRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+    }
+}
